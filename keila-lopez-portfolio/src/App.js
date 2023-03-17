f342491fec4c396d2f8414abcb1191a65ba95880
Keila Lopez <keilalofra@gmail.com>
2023-03-17 10:59:25 -0600

    Added header component

--- keila-lopez-portfolio/src/App.js
+++ keila-lopez-portfolio/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>keila</h1>
+
     </div>
   );
 }
