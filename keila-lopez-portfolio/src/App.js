a0b9bb207d45ff2c3a60a15a96743438122f83f9
Keila Lopez <keilalofra@gmail.com>
2023-03-21 09:13:29 -0600

    Added skills section

--- keila-lopez-portfolio/src/App.js
+++ keila-lopez-portfolio/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Components/Header'
 import About from './Components/About'
 import Projects from './Components/Projects'
+import Skills from './Components/Skills'
 import Certificates from './Components/Certificates'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <About />
         <Projects />
+        <Skills />
         <Certificates />
       </div>
     </>
