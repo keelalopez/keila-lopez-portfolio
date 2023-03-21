ad7d15aeb9fbdcf101ad1e75ffaa29d0140b4b2b
Keila Lopez <keilalofra@gmail.com>
2023-03-20 21:40:13 -0600

    Added certificates component

--- keila-lopez-portfolio/src/App.js
+++ keila-lopez-portfolio/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Components/Header'
 import About from './Components/About'
 import Projects from './Components/Projects'
+import Certificates from './Components/Certificates'
 
 function App() {
   return (
---
+++ keila-lopez-portfolio/src/Components/Certificates.js
@@ -0,0 +1,11 @@
+
+
+function Certificates () {
+    return (
+        <div>
+            <p>Certificates</p>
+        </div>
+    )
+}
+
+export default Certificates;
