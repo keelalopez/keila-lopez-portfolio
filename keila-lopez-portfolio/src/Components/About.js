a47459c3ee68d945b338438929d4f60a2a6b6844
Keila Lopez <keilalofra@gmail.com>
2023-03-24 12:32:50 -0600

    Updated styling for about section

--- keila-lopez-portfolio/src/Components/About.js
+++ keila-lopez-portfolio/src/Components/About.js
@@ -2,7 +2,15 @@
 function About () {
     return (
         <div className="about-container">
-            <h1>About section</h1>
+            <div className="bio">
+                <p>A Software Engineer with a professional background in General Contracting and an academic background in Civil Engineering.</p>
+                <p>My professional background is in Civil Engineering and Construction Management. After working for a general contractor for half a decade, teamwork and collaborative problem solving drive my approach to critical thinking. Responsible for the day-to-day maintenance of the project information flow and their respective documentation, as well as establishing and updating the quality control protocols for a project. Adaptive team player who held positions in five divisions at my last company, and moved projects and teams as needed. I used Bluebeam, BIM Docs, BIM 360, Procore, OpenSpace, and CMiC as a Project Engineer.</p>
+                <p>As a recent bootcamp graduate, I have experience in Object-Oriented Programming, MVC, debugging, and RESTful APIs. Full stack Software Engineer fluent in JavaScript, React.js, Ruby, Ruby on Rails, ActiveRecord, HTML, CSS, Semantic UI, SQL, Sinatra, PostgreSQL, Git/GitHub, wireframing, and full-stack web development.</p>
+                <p>Currently using Netlify to deploy static websites. Learning Typescript and Data Structures and Algorithms</p>
+            </div>
+            <div>
+                <h1>About Me</h1>
+            </div>
         </div>
     )
 }
