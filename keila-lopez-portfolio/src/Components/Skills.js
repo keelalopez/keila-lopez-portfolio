2b9d0025fed95e3dfcd1c163de4183ea9fd0f10a
Keila Lopez <keilalofra@gmail.com>
2023-03-24 12:39:56 -0600

    Styled skills section

--- keila-lopez-portfolio/src/Components/Skills.js
+++ keila-lopez-portfolio/src/Components/Skills.js
@@ -1,8 +1,25 @@
 
 function Skills () {
     return (
-        <div className="skills-container">
-            Skills
+        <div>
+            <h1>Skills</h1>
+            <div className="skills-container">
+                <img />
+            </div>
+            <div>
+                <div className="skills-detail-div">
+                    <h1>Proficient</h1>
+                    <p>JavaScript | React | Rails | </p>
+                </div>
+                <div className="skills-detail-div">
+                    <h1>Learning</h1>
+                    <p>TypeScript | Redux | DSA</p>
+                </div>
+                <div className="skills-detail-div">
+                    <h1>Resume</h1>
+                    <p>Download Resume </p>
+                </div>
+            </div>
         </div>
     )
 }
