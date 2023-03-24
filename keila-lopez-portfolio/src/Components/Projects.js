e8156187fe8fc5799f42773a370883b2a39e3fbc
Keila Lopez <keilalofra@gmail.com>
2023-03-24 12:36:24 -0600

    Updated project section styling

--- keila-lopez-portfolio/src/Components/Projects.js
+++ keila-lopez-portfolio/src/Components/Projects.js
@@ -1,13 +1,28 @@
 
 function Projects () {
     return (
-        <div className="project-container">
-            <div className="project-card">
-                <p>Repo | Demo</p>
-                <img />
-                <h3>Brix</h3>
-                <p>A Fullstack procurement application</p>
-            </div>
+        <div>
+            <h1>Projects</h1>
+            <div className="project-container">
+                <div className="project-card">
+                    <p>Repo | Demo</p>
+                    <img />
+                    <h3>Brix</h3>
+                    <p>A Fullstack procurement application</p>
+                </div>
+                <div className="project-card">
+                    <p>Repo | Demo</p>
+                    <img />
+                    <h3>Dough</h3>
+                    <p>Full stack expense tracker application</p>
+                </div>
+                <div className="project-card">
+                    <p>Repo | Demo</p>
+                    <img />
+                    <h3>Casa Maya</h3>
+                    <p>Full stack tequila e-commerce site</p>
+                </div>
+        </div>
         </div>
     )
 }
