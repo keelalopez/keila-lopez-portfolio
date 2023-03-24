d8e7377639a6015f7addd024210454225072a1c0
Keila Lopez <keilalofra@gmail.com>
2023-03-24 12:12:25 -0600

    updated cover component to use flexbox

--- keila-lopez-portfolio/src/Components/Cover.js
+++ keila-lopez-portfolio/src/Components/Cover.js
@@ -2,9 +2,11 @@
 function Cover () {
     return (
         <div className="cover-container">
-            <h1>Hi I'm Keila</h1>
             <img src="https://i.imgur.com/QgSWGOZ.jpg"/>
-            <h1>Software Engineer</h1>
+            <div>
+                <h1>Hi I'm Keila</h1>
+                <h1>Software Engineer</h1>
+            </div>
         </div>
     )
 }
