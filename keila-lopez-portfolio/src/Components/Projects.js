4c879a613aa20111f3f2c59eb9b5d197fdc092ae
Keila Lopez <keilalofra@gmail.com>
2023-03-24 11:21:39 -0600

    Added project divs

--- keila-lopez-portfolio/src/Components/Projects.js
+++ keila-lopez-portfolio/src/Components/Projects.js
@@ -1,8 +1,16 @@
 
 function Projects () {
     return (
-        <div>
-            Projects
+        <div className="project-container">
+            <div>
+                Brix
+            </div>
+            <div>
+                Dough
+            </div>
+            <div>
+                Casa Maya
+            </div>
         </div>
     )
 }
