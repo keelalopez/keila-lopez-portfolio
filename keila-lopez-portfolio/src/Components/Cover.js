73ea5917e8add2e9febe379a13a3cf7da2c4d61d
Keila Lopez <keilalofra@gmail.com>
2023-03-24 12:07:57 -0600

    updated cover component. added image

--- keila-lopez-portfolio/src/Components/Cover.js
+++ keila-lopez-portfolio/src/Components/Cover.js
@@ -2,7 +2,8 @@
 function Cover () {
     return (
         <div className="cover-container">
-            <h1>Keila Lopez</h1>
+            <h1>Hi I'm Keila</h1>
+            <img src="https://i.imgur.com/QgSWGOZ.jpg"/>
             <h1>Software Engineer</h1>
         </div>
     )
